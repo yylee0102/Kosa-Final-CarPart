9a889b104841991bad620842236f3c4410fca8e2
MasterLee <39284552+yylee0102@users.noreply.github.com>
2025-09-24 18:03:22 +0900

    [Fix] CsInquiry생성하는 부분 수정
    makeCsInquiry 서비스 반환값을 CsInquiry에서 CsInquiryResDTO 로 바꿨습니다.

--- carparter/src/main/java/com/spring/carparter/service/HelpService.java
+++ carparter/src/main/java/com/spring/carparter/service/HelpService.java
@@ -10,5 +10,12 @@ public class HelpService {
     {
         // 진단 도우미 서비스(챗봇)
 
+        // 자동차 등록 APi
+
+        // 네이버 sens
+
+        // FireBase (이미지)
+
+        // python을 직접적으로 react 로 연결하는 것이 낫다.
     }
 }
